21edc9fea2f71e3e153e6817335cc26b7afef774
Nadipannagari Anilkumar <111619322+anilnadipannagari@users.noreply.github.com>
2023-02-21 22:40:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let username=document.getElementById("inp");
 let pasword=document.getElementById('pwd')
 
 let submitbtn=document.getElementById('submitbtn');
-
+let intro=document.createElement('h1').vlaue='Enter user id and Pasword';
+body.append(intro);
 function validation(){
 if(username.value !="" && pasword.value !=""){
     alert("Your detailes saved sucessfully");
